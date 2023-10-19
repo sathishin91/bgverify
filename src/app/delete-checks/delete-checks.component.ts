79cb5ee8e983353e51d4444ecccea3ceaa353076
vivekaviva <vivekaloganathan37@gmail.com>
2023-10-19 12:32:14 +0530

    with the delete page started

--- src/app/delete-checks/delete-checks.component.ts
+++ src/app/delete-checks/delete-checks.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-delete-checks',
   templateUrl: './delete-checks.component.html',
-  styleUrls: ['./delete-checks.component.scss']
+  styleUrls: ['./delete-checks.component.scss'],
 })
-export class DeleteChecksComponent {
-
-}
+export class DeleteChecksComponent {}

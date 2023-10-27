5e5863cf767130b049e5652e1472b277eea6a303
vivekaviva <vivekaloganathan37@gmail.com>
2023-10-27 20:53:10 +0530

    UPDATED CODE

--- src/app/all-clients/verify-services/verify/verify.component.ts
+++ src/app/all-clients/verify-services/verify/verify.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-verify',
   templateUrl: './verify.component.html',
-  styleUrls: ['./verify.component.scss']
+  styleUrls: ['./verify.component.scss'],
 })
 export class VerifyComponent {
+  mappingService = false;
+  tatMappingServices = false;
+  betterPlaceSpocServices = false;
+  openMappingServices() {
+    this.mappingService = true;
+  }
 
+  openTatMappingServices() {
+    this.tatMappingServices = true;
+    this.mappingService = false;
+  }
+
+  openBetterPlaceSpocServices() {
+    this.betterPlaceSpocServices = true;
+    this.tatMappingServices = false;
+  }
 }
